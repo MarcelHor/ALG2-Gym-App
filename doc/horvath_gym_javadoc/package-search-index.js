c16426cea665a00a952d79030c179736fd0ef48e
Marcel Horvath <marcel.horvath@tul.cz>
2022-05-31 22:44:06 +0200

    update

--- doc/horvath_gym_javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"horvath.app"},{"l":"horvath.gym"}];updateSearchResults();
--- doc/horvath_gym_javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"horvath.gym","l":"GymTools.Gender"},{"p":"horvath.gym","l":"Gym"},{"p":"horvath.app","l":"GymApp"},{"p":"horvath.gym","l":"GymException"},{"p":"horvath.app","l":"GymPrompt"},{"p":"horvath.gym","l":"GymTools"},{"p":"horvath.gym","l":"User"}];updateSearchResults();
